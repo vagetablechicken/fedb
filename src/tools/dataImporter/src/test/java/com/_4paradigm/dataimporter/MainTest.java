05f974c05de0f7b890afac8cd31a3cd99981b4d6
HuangWei <huangwei@apache.org>
2021-07-08 16:33:24 +0800

    tmp BulkLoadInfoResponse design

---
+++ src/tools/dataImporter/src/test/java/com/_4paradigm/dataimporter/MainTest.java
@@ -0,0 +1,12 @@
+package com._4paradigm.dataimporter;
+
+import junit.framework.TestCase;
+
+// TODO(hw): run this test instead of running main class, when using system scope dependencies
+public class MainTest extends TestCase {
+
+    public void testBulkLoad(){
+        Main.main(null);
+    }
+
+}
