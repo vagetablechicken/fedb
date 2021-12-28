96a877ca46acc32fef5e7905dd15a400a5374fe1
HuangWei <huangwei@apache.org>
2021-12-28 19:03:28 +0800

    []

--- java/openmldb-batch/src/main/scala/com/_4paradigm/openmldb/batch/nodes/LoadDataPlan.scala
+++ java/openmldb-batch/src/main/scala/com/_4paradigm/openmldb/batch/nodes/LoadDataPlan.scala
@@ -18,7 +18,7 @@ package com._4paradigm.openmldb.batch.nodes
 import com._4paradigm.hybridse.node.ConstNode
 import com._4paradigm.hybridse.sdk.UnsupportedHybridSeException
 import com._4paradigm.hybridse.vm.PhysicalLoadDataNode
-import com._4paradigm.openmldb.batch.utils.SparkRowUtil
+import com._4paradigm.openmldb.batch.utils.{HybridseUtil, SparkRowUtil}
 import com._4paradigm.openmldb.batch.{OpenmldbBatchConfig, PlanContext, SparkInstance}
 import com._4paradigm.openmldb.proto.NS.OfflineTableInfo
 import com._4paradigm.openmldb.proto.Type.DataType
@@ -31,75 +31,6 @@ import scala.collection.mutable
 object LoadDataPlan {
   private val logger = LoggerFactory.getLogger(this.getClass)
 
-  def getStr(node: ConstNode): String = {
-    node.GetStr()
-  }
-
-  def getBool(node: ConstNode): String = {
-    node.GetBool().toString
-  }
-
-  def getStringOrDefault(node: ConstNode, default: String): String = {
-    if (node != null) {
-      node.GetStr()
-    } else {
-      default
-    }
-  }
-
-  def getBoolOrDefault(node: ConstNode, default: String): String = {
-    if (node != null) {
-      node.GetBool().toString
-    } else {
-      default
-    }
-  }
-
-  def parseOption(node: ConstNode, default: String, f: (ConstNode, String) => String): String = {
-    f(node, default)
-  }
-
-  def updateOptionsMap(options: mutable.Map[String, String], node: ConstNode, name: String, getValue: ConstNode =>
-    String): Unit = {
-    if (node != null) {
-      options += (name -> getValue(node))
-    }
-  }
-
-  def parseOptions(node: PhysicalLoadDataNode): (String, Map[String, String], String, Boolean) = {
-    // read format
-    val format = parseOption(node.GetOption("format"), "csv", getStringOrDefault).toLowerCase
-    require(format.equals("csv") || format.equals("parquet"))
-
-    // read options
-    val options: mutable.Map[String, String] = mutable.Map()
-    // default values:
-    // delimiter -> sep: ,
-    // header: true(different with spark)
-    // null_value -> nullValue: null(different with spark)
-    // quote: '\0'(means no quote, the same with spark quote "empty string")
-    options += ("header" -> "true")
-    options += ("nullValue" -> "null")
-    updateOptionsMap(options, node.GetOption("delimiter"), "sep", getStr)
-    updateOptionsMap(options, node.GetOption("header"), "header", getBool)
-    updateOptionsMap(options, node.GetOption("null_value"), "nullValue", getStr)
-    updateOptionsMap(options, node.GetOption("quote"), "quote", getStr)
-
-    // write mode/save mode, not the read mode
-    val modeStr = parseOption(node.GetOption("mode"), "error_if_exists", getStringOrDefault).toLowerCase
-    val mode = modeStr match {
-      case "error_if_exists" => "errorifexists"
-      // append/overwrite, stay the same
-      case "append" | "overwrite" => modeStr
-      case others: Any => throw new UnsupportedHybridSeException(s"unsupported write mode $others")
-    }
-
-    // if symbolic link(aka deep_copy)
-    val deepCopy = parseOption(node.GetOption("deep_copy"), "true", getBoolOrDefault).toBoolean
-
-    (format, options.toMap, mode, deepCopy)
-  }
-
   def gen(ctx: PlanContext, node: PhysicalLoadDataNode): SparkInstance = {
     val inputFile = node.File()
     val db = if (node.Db().nonEmpty) node.Db() else ctx.getConf.defaultDb
@@ -111,8 +42,10 @@ object LoadDataPlan {
     require(storage == "offline" || storage == "online")
 
     // read settings
-    val (format, options, mode, deepCopy) = parseOptions(node)
-    logger.info("load data to storage {}, read[format {}, options {}], write[mode {}], is soft? {}", storage, format,
+    val (format, options, mode, deepCopyOpt) = HybridseUtil.parseOptions(node)
+    require(deepCopyOpt.nonEmpty)
+    val deepCopy = deepCopyOpt.get
+    logger.info("load data to storage {}, read[format {}, options {}], write[mode {}], is deep? {}", storage, format,
       options, mode, deepCopy.toString)
 
     require(ctx.getOpenmldbSession != null, "LOAD DATA must use OpenmldbSession, not SparkSession")
@@ -122,7 +55,7 @@ object LoadDataPlan {
 
     // write
     if (storage == "online") {
-      require(deepCopy && mode == "append", "import to online storage, can't do deep copy, and mode must be append")
+      require(deepCopy && mode == "append", "import to online storage, must be deep copy, and the mode must be append")
 
       val writeOptions = Map("db" -> db, "table" -> table,
         "zkCluster" -> ctx.getConf.openmldbZkCluster,
---
+++ java/openmldb-batch/src/test/scala/com/_4paradigm/openmldb/batch/TestSelectIntoPlan.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2021 4Paradigm
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com._4paradigm.openmldb.batch
+
+import com._4paradigm.openmldb.batch.api.OpenmldbSession
+import com._4paradigm.openmldb.sdk.impl.SqlClusterExecutor
+import org.apache.spark.sql.{AnalysisException, Row}
+import org.apache.spark.sql.types.{DoubleType, IntegerType, LongType, StructField, StructType}
+
+import scala.collection.JavaConverters.seqAsJavaListConverter
+
+class TestSelectIntoPlan extends SparkTestSuite {
+  test("Test Plan Select Into") {
+    val sess = getSparkSession
+
+    val schema = StructType(Seq(
+      StructField("id", IntegerType),
+      StructField("time2", LongType),
+      StructField("amt", DoubleType)
+    ))
+
+    val t1 = sess.createDataFrame(Seq(
+      (0, 1L, 1.0),
+      (3, 3L, 3.0),
+      (2, 10L, 4.0),
+      (0, 2L, 2.0),
+      (2, 13L, 3.0)
+    ).map(Row.fromTuple(_)).asJava, schema)
+
+    val planner = new SparkPlanner(sess)
+    val filePath = "/tmp/select_into_test"
+    val res = planner.plan(s"select id from t1 into outfile '$filePath' " +
+      "options(format='csv', foo='bar', header=false, mode='overwrite');", Map("t1" -> t1))
+    res.getDf().show()
+    try {
+      // writing in default mode 'errorifexsits' will get exception, cuz filePath contains data
+      planner.plan(s"select id from t1 into outfile '$filePath' " +
+        "options(format='csv', foo='bar', header=false);", Map("t1" -> t1))
+    } catch {
+      case e: AnalysisException => println("It should catch this: " + e.toString)
+    }
+
+    // read data is disordered, so only check the count
+    val df = getSparkSession.read.schema("id int").csv(filePath)
+    assert(df.count() == t1.count())
+  }
+}
